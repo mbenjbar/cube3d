980ddaa389a74d9ad95cd655131015051c1d3e70
Mohamed Benjbara <mbenjbar@c3r8p7.1337.ma>
2025-10-18 23:33:10 +0100

    map does not look totally perfect

--- play/move.c
+++ play/move.c
@@ -6,37 +6,37 @@
 /*   By: mbenjbar <mbenjbar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 21:26:57 by mbenjbar          #+#    #+#             */
-/*   Updated: 2025/10/18 23:24:07 by mbenjbar         ###   ########.fr       */
+/*   Updated: 2025/10/18 23:32:02 by mbenjbar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header.h"
 
-void	vertical(t_game *game, double *x, double *y)
+void vertical(t_game *game, double *x, double *y)
 {
 	if (game->key_up == true)
 	{
-		*(x) += (STEP * cos(game->angle));
-		*(y) -= (STEP * sin(game->angle));
+		*x += STEP * cos(game->angle);
+		*y -= STEP * sin(game->angle);
 	}
 	if (game->key_down == true)
 	{
-		*(x) -= (STEP * cos(game->angle));
-		*(y) += (STEP * sin(game->angle));
+		*x -= STEP * cos(game->angle);
+		*y += STEP * sin(game->angle);
 	}
 }
 
-void	horizontal(t_game *game, double *x, double *y)
+void horizontal(t_game *game, double *x, double *y)
 {
 	if (game->key_left == true)
 	{
-		*(x) -= sin(game->angle) * STEP;
-		*(y) -= cos(game->angle) * STEP;
+		*x -= STEP * sin(game->angle);
+		*y -= STEP * cos(game->angle);
 	}
 	if (game->key_right == true)
 	{
-		*(x) += sin(game->angle) * STEP;
-		*(y) += cos(game->angle) * STEP;
+		*x += STEP * sin(game->angle);
+		*y += STEP * cos(game->angle);
 	}
 }
 
